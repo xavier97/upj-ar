f5562360dd851226cb6dd6584c472c7587e63f45
Adam <afeather20@gmail.com>
2018-03-30 23:39:13 -0400

    I got the AR working with camera background next step is gettting the cube in place of the plane and getting the QR to work

--- UPJAR/Screens/GameViewController.cs
+++ UPJAR/Screens/GameViewController.cs
@@ -7,12 +7,14 @@ using UIKit;
 
 namespace UPJAR
 {
+    
     public partial class GameViewController : UIViewController
     {
+
+      
+
         private ARSCNView sceneView;
         protected GameViewController(IntPtr handle) : base(handle) { }
-
-
 
         public override void DidReceiveMemoryWarning()
         {
@@ -20,19 +22,25 @@ namespace UPJAR
             // Release any cached data, images, etc that aren't in use.
         }
 
+        public override bool ShouldAutorotate() => true;        
+     
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
 
-            sceneView = new ARSCNView
+
+            sceneView = new ARSCNView()
             {
                 Frame = View.Frame,
                 DebugOptions = ARSCNDebugOptions.ShowFeaturePoints |
-          ARSCNDebugOptions.ShowWorldOrigin,
+                ARSCNDebugOptions.ShowWorldOrigin,
                 UserInteractionEnabled = true
             };
 
-            View.AddSubview(sceneView);
+            //View.AddSubview(sceneView);
+
+           
+           
         }
 
     
@@ -40,14 +48,97 @@ namespace UPJAR
         {
             base.ViewWillAppear(animated);
 
+
             var configuration = new ARWorldTrackingConfiguration
             {
                 PlaneDetection = ARPlaneDetection.Horizontal,
-                LightEstimationEnabled = true
+                LightEstimationEnabled = true,
+
+                    
             };
+            configuration.PlaneDetection = ARPlaneDetection.Horizontal;
+            sceneView.Scene = SCNScene.FromFile("art.scnassets/ship");
+            var ship = sceneView.Scene.RootNode.FindChildNode("ship", true);
+            ship.Position = new SCNVector3(0f, -2f, -9f);
+            ship.Scale = new SCNVector3(1f, 1f, 1f);
+           
 
             sceneView.Session.Run(configuration, ARSessionRunOptions.ResetTracking |
-              ARSessionRunOptions.RemoveExistingAnchors);
+            ARSessionRunOptions.RemoveExistingAnchors);
+
+         
+            var scnView = (SCNView)View;
+
+            scnView.Scene = SCNScene.FromFile("art.scnassets/ship");
+
+
+            // allows the user to manipulate the camera
+            scnView.AllowsCameraControl = true;
+
+            // show statistics such as fps and timing information
+            scnView.ShowsStatistics = true;
+
+            // add a tap gesture recognizer
+            var tapGesture = new UITapGestureRecognizer(HandleTap);
+
+            var gestureRecognizers = new List<UIGestureRecognizer>();
+
+            gestureRecognizers.Add(tapGesture);
+
+
+            gestureRecognizers.AddRange(scnView.GestureRecognizers);
+            scnView.GestureRecognizers = gestureRecognizers.ToArray();
+
+          
+            
+        }
+        void HandleTap(UIGestureRecognizer gestureRecognize)
+        {
+            Console.WriteLine("touch");
+            // retrieve the SCNView
+            var scnView = (SCNView)View;
+          
+
+            // check what nodes are tapped
+            CGPoint p = gestureRecognize.LocationInView(scnView);
+            SCNHitTestResult[] hitResults = scnView.HitTest(p, (SCNHitTestOptions)null);
+
+            // check that we clicked on at least one object
+            Console.WriteLine(hitResults.Length);
+            if (hitResults.Length  > 0)
+            {
+                // retrieved the first clicked object
+                SCNHitTestResult result = hitResults[0];
+
+                // get its material
+                SCNMaterial material = result.Node.Geometry.FirstMaterial;
+
+                // highlight it
+                SCNTransaction.Begin();
+                SCNTransaction.AnimationDuration = 0.5f;
+
+                // on completion - unhighlight
+                SCNTransaction.SetCompletionBlock(() =>
+                {
+                    SCNTransaction.Begin();
+                    SCNTransaction.AnimationDuration = 0.5f;
+
+
+
+                    SCNTransaction.Commit();
+                });
+
+                material.Emission.Contents = UIColor.Red;
+
+                SCNTransaction.Commit();
+            }
+        }
+
+
+
+        public override UIInterfaceOrientationMask GetSupportedInterfaceOrientations()
+        {
+            return UIInterfaceOrientationMask.AllButUpsideDown;
         }
 
         public override void ViewDidDisappear(bool animated)
