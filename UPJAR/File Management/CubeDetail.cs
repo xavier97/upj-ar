a9cc0e0ffd9a670097bcd9414e8eef2588afbd3d
Xavier <xad2@pitt.edu>
2018-04-14 20:50:50 -0400

    fixed cube detail to match json

--- UPJAR/File Management/CubeDetail.cs
+++ UPJAR/File Management/CubeDetail.cs
@@ -5,29 +5,29 @@ namespace UPJAR
     public class CubeDetail
     {
         public string name { get; set; }
-        public string description { get; set; }
-        public string assetLocation { get; set; }
-        public double GPSLat { get; set; }
-        public double GPSLong { get; set; }
-        public string QRCode { get; set; }
-        public string img1 { get; set; }
-        public string img2 { get; set; }
-        public string img3 { get; set; }
-        public string img4 { get; set; }
-        public string img5 { get; set; }
-        public string img6 { get; set; }
-        public string img7 { get; set; }
-        public string img8 { get; set; }
-        public string img9 { get; set; }
-        public string img10 { get; set; }
-        public string img11 { get; set; }
-        public string img12 { get; set; }
+        public string desc { get; set; }
+        public string asset { get; set; }
+        public string Lat { get; set; }
+        public string Long { get; set; }
+        public string timestamp { get; set; }
+        public string image1 { get; set; }
+        public string image2 { get; set; }
+        public string image3 { get; set; }
+        public string image4 { get; set; }
+        public string image5 { get; set; }
+        public string image6 { get; set; }
+        public string image7 { get; set; }
+        public string image8 { get; set; }
+        public string image9 { get; set; }
+        public string image10 { get; set; }
+        public string image11 { get; set; }
+        public string image12 { get; set; }
 
-		public override string ToString()
+        public override string ToString()
 		{
-            return string.Format("{0} {1} {2} {3} {4} {5} {6} {7} {8}",
-                                 name, description, assetLocation, GPSLat, GPSLong, QRCode,
-                                 img1, img2, img3);
+            return string.Format("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13} {14} {15} {16} {17}",
+                                 name, desc, asset, Lat, Long, timestamp,
+                                 image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12);
 		}
 	}
 }
