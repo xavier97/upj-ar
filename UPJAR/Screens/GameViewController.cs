fe9d5af8c1e989d145634660b388efc370ceb883
Adam <afeather20@gmail.com>
2018-04-25 03:07:49 -0400

    added alert view

--- UPJAR/Screens/GameViewController.cs
+++ UPJAR/Screens/GameViewController.cs
@@ -59,11 +59,11 @@ namespace UPJAR
                                   "Make sure enough images are in the specified folder (determined by key var).", e.Message);
             }
 
-            SCNMaterial[] joe = new SCNMaterial[] { a, b,  c,d,placeholder, placeholder};
+            SCNMaterial[] joe = new SCNMaterial[] { a, b, c, d, placeholder, placeholder };
             // This demo was originally in F# :-)   
             return joe;
         }
-       
+
         private SCNMaterial[] LoadMaterials2()
         {
 
@@ -96,7 +96,7 @@ namespace UPJAR
                                   "Make sure enough images are in the specified folder (determined by key var).", e.Message);
             }
 
-            SCNMaterial[] joe = new SCNMaterial[] {  a, b,  c, d,placeholder, placeholder };
+            SCNMaterial[] joe = new SCNMaterial[] { a, b, c, d, placeholder, placeholder };
             // This demo was originally in F# :-)   
             return joe;
         }
@@ -132,7 +132,7 @@ namespace UPJAR
                                   "Make sure enough images are in the specified folder (determined by key var).", e.Message);
             }
 
-            SCNMaterial[] joe = new SCNMaterial[] {   a,b,  c, d, placeholder, placeholder};
+            SCNMaterial[] joe = new SCNMaterial[] { a, b, c, d, placeholder, placeholder };
             // This demo was originally in F# :-)   
             return joe;
         }
@@ -153,7 +153,7 @@ namespace UPJAR
             var scanner = new ZXing.Mobile.MobileBarcodeScanner();
 
             var result = await scanner.Scan();
-           
+
             // if result matches location from json, pull up spec. qr; else reload qr scanner
             if (result != null)
             {
@@ -161,11 +161,11 @@ namespace UPJAR
                 while (count < assetList.Count)
                 {
                     Console.WriteLine(count);
-                  
+
 
                     if (result.Text == assetList[count].name)
                     {
-                       
+
 
                         assetKey = count;
                         Title = assetList[count].name;
@@ -179,7 +179,8 @@ namespace UPJAR
                     count++;
                 }
 
-                if(assetKey == -1){
+                if (assetKey == -1)
+                {
                     assetKey = -2;
                 }
 
@@ -187,119 +188,129 @@ namespace UPJAR
             Console.WriteLine(assetKey);
             if (assetKey == -1)
             {
-                
-                    
-                 NavigationController.PopViewController(true);
-               
 
 
-               
+                NavigationController.PopViewController(true);
+
+
+
+
             }
-            else if(assetKey == -2){
-                
+            else if (assetKey == -2)
+            {
+
                 var alert1 = UIAlertController.Create("QR Invalid", "The QR Code you scanned is invalid please use QR codes located at tour locations", UIAlertControllerStyle.Alert);
                 alert1.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Cancel, alert => NavigationController.PopViewController(true)));
                 PresentViewController(alert1, true, null);
             }
-            else{
-                
-           
-            sceneView = new ARSCNView
+            else
             {
-                Frame = View.Frame,
-                DebugOptions = ARSCNDebugOptions.ShowFeaturePoints |
-                ARSCNDebugOptions.ShowWorldOrigin,
-                UserInteractionEnabled = true
-            };
+                var alert2 = UIAlertController.Create("AR Tour Activated", "Please rotate the phone around the room to find AR Cubes to explore the area", UIAlertControllerStyle.Alert);
+                var imgTitle = new UIImage();
+                imgTitle = UIImage.FromFile("art.scnassets / download.jpg");
+                var imgViewTitle = new UIImageView(new CGRect(x: 10, y: 10, width: 30, height: 30));
+                imgViewTitle.Image = imgTitle;
+                alert2.View.AddSubview(imgViewTitle);
+                alert2.View.AddSubview(imgViewTitle);
+                alert2.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Cancel, null));
+                PresentViewController(alert2, true, null);
 
-            View.AddSubview(sceneView);
+                sceneView = new ARSCNView
+                {
+                    Frame = View.Frame,
+                    //DebugOptions = ARSCNDebugOptions.ShowFeaturePoints |
+                    //ARSCNDebugOptions.ShowWorldOrigin,
+                    UserInteractionEnabled = true
+                };
 
-            // TODO: TEST THIS
-            AddFooter(); // Add footer with buttons
+                View.AddSubview(sceneView);
+
+                // TODO: TEST THIS
+                AddFooter(); // Add footer with buttons
 
                 var configuration = new ARWorldTrackingConfiguration
                 {
-                    PlaneDetection  = ARPlaneDetection.Horizontal,
+                    PlaneDetection = ARPlaneDetection.Horizontal,
                     LightEstimationEnabled = true
-            };
+                };
 
-            configuration.PlaneDetection = ARPlaneDetection.Horizontal;
-            sceneView.Scene = new SCNScene();
+                configuration.PlaneDetection = ARPlaneDetection.Horizontal;
+                sceneView.Scene = new SCNScene();
 
-            var material = new SCNMaterial();
+                var material = new SCNMaterial();
 
-            // Determine types of texture to use, based on data sent by QR screen
-            int key = assetKey;
-            string textureFolderPath = path + "/asset" + key;
+                // Determine types of texture to use, based on data sent by QR screen
+                int key = assetKey;
+                string textureFolderPath = path + "/asset" + key;
 
-            string[] Files = Directory.GetFiles(textureFolderPath, "*.png"); // Getting Text files
+                string[] Files = Directory.GetFiles(textureFolderPath, "*.png"); // Getting Text files
 
-            for (int assetCount = 0; assetCount < Files.Length; assetCount++)
-            {
-                material.Diffuse.Contents = UIImage.FromFile(path + "/" + Files[assetCount]);
-            }
-
-
-            material.LocksAmbientWithDiffuse = true;
+                for (int assetCount = 0; assetCount < Files.Length; assetCount++)
+                {
+                    material.Diffuse.Contents = UIImage.FromFile(path + "/" + Files[assetCount]);
+                }
 
 
-            var ship = new SCNNode();
+                material.LocksAmbientWithDiffuse = true;
 
 
-            ship.Geometry = new SCNBox
-            {
-                Width = 1,
-                Height = 1,
-                Length = 1,
-            };
+                var ship = new SCNNode();
 
 
-            ship.Geometry.Materials = LoadMaterials();
+                ship.Geometry = new SCNBox
+                {
+                    Width = 1,
+                    Height = 1,
+                    Length = 1,
+                };
 
 
-            ship.Position = new SCNVector3(1f, 0f, 0f);
-            ship.Scale = new SCNVector3(.5f, .5f, .5f);
-
-            sceneView.Scene.RootNode.AddChildNode(ship);
+                ship.Geometry.Materials = LoadMaterials();
 
 
+                ship.Position = new SCNVector3(1f, 0f, 0f);
+                ship.Scale = new SCNVector3(.5f, .5f, .5f);
 
-            var ship2 = ship.Clone();
-
-            ship2.Geometry = new SCNBox
-            {
-                Width = 1,
-                Height = 1,
-                Length = 1,
+                sceneView.Scene.RootNode.AddChildNode(ship);
 
 
-            };
-            
+
+                var ship2 = ship.Clone();
+
+                ship2.Geometry = new SCNBox
+                {
+                    Width = 1,
+                    Height = 1,
+                    Length = 1,
+
+
+                };
+
                 var ship3 = ship.Clone();
 
-            ship3.Geometry = new SCNBox
-            {
-                Width = 1,
-                Height = 1,
-                Length = 1,
+                ship3.Geometry = new SCNBox
+                {
+                    Width = 1,
+                    Height = 1,
+                    Length = 1,
 
 
-            };
+                };
 
-            ship2.Geometry.Materials = LoadMaterials2();
+                ship2.Geometry.Materials = LoadMaterials2();
 
-            sceneView.Scene.RootNode.AddChildNode(ship2);
-            
-            ship2.Position = new SCNVector3(-1f, 0f, 1f);
+                sceneView.Scene.RootNode.AddChildNode(ship2);
 
-            ship3.Geometry.Materials = LoadMaterials3();
+                ship2.Position = new SCNVector3(-1f, 0f, 1f);
 
-            sceneView.Scene.RootNode.AddChildNode(ship3);
+                ship3.Geometry.Materials = LoadMaterials3();
 
-            ship3.Position = new SCNVector3(-3f, 0f, 0f);
-      
-            sceneView.Session.Run(configuration, ARSessionRunOptions.ResetTracking );
-            
+                sceneView.Scene.RootNode.AddChildNode(ship3);
+
+                ship3.Position = new SCNVector3(-3f, 0f, 0f);
+
+                sceneView.Session.Run(configuration, ARSessionRunOptions.ResetTracking);
+
             }
         }
 
@@ -308,21 +319,21 @@ namespace UPJAR
         /// </summary>
         private void AddFooter()
         {
-           
+
 
             string textureFolderPath = path + "/asset" + assetKey;
             string[] Files = Directory.GetFiles(textureFolderPath, "*.mp3");
 
             url = NSUrl.FromFilename(Files[0]);
             thhing = AVAudioPlayer.FromUrl(url);
-           
+
 
             // Make footer buttons here.
             this.SetToolbarItems(new UIBarButtonItem[] {
                 new UIBarButtonItem(UIBarButtonSystemItem.Play, (s,e) =>
                 {
 
-                   
+
                      if (thhing.Playing != true)
                     {
                         thhing.Play();
@@ -352,40 +363,40 @@ namespace UPJAR
                         View.InvokeOnMainThread(() =>
                         {
                         loadPop.Hide();
-                         });                   
+                         });
                     }
-                  
+
                         })
                     }, false);
 
             this.NavigationController.ToolbarHidden = false;
         }
 
-		public override void DidReceiveMemoryWarning()
+        public override void DidReceiveMemoryWarning()
         {
-           
+
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
-           
+
         }
 
         public override void ViewDidLoad()
         {
-           
+
             base.ViewDidLoad();
-           
-            RefreshDataAsync(); 
+
+            RefreshDataAsync();
 
 
 
 
         }
 
-		public override void ViewWillDisappear(bool animated)
-		{
+        public override void ViewWillDisappear(bool animated)
+        {
 
             //Turns off the sound playing if the view disapears
-          
+
             if (thhing != null)
             {
                 if (thhing.Playing == true)
@@ -393,13 +404,13 @@ namespace UPJAR
                     thhing.Stop();
                 }
             }
-			base.ViewWillDisappear(animated);
-		}
-		public override void ViewWillAppear(bool animated)
+            base.ViewWillDisappear(animated);
+        }
+        public override void ViewWillAppear(bool animated)
         {
-            
+
             base.ViewWillAppear(animated);
-           
+
         }
     }
 }
